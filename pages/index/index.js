f32cb8b178a3ff030e60fa48eaba65bf000de232
LiAo <1412853895@qq.com>
2019-11-27 20:59:12 +0800

    主页布局结束

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,16 +12,39 @@
  */
 Page({
   data:{
-    imgsrc:[]
+    //轮播图
+    imgsrc:[],
+    //分类数据
+    fenlei:[],
+    //楼层数据
+    louceng:[]
   },
 onLoad(){
+  //轮播图
   wx.request({
     url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
     success: (result) => {
-      console.log(result.data);
+      // console.log(result.data);
       this.setData({imgsrc:result.data.message})
     }
   });
+  //导航数据
+  wx.request({
+    url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+    success: (result) => {
+      // console.log(result.data.message);
+      this.setData({fenlei:result.data.message})
+    }
+  });
+  //分类数据
+  wx.request({
+    url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+    success: (result) => {
+      console.log(result.data.message);
+      this.setData({louceng:result.data.message})
+    }
+  });
+     
     
 }
 })
